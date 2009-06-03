fb3f517bdcb2df99018197e93b845af62d4b319a
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-06-03 20:34:00 +0200

    Simpler implementation of LISP-OBJ-GET-METADATA ... and METEDATA-OF (and setf) added and exported.

--- util/lisp-object-metadata.lisp
+++ util/lisp-object-metadata.lisp
@@ -2,37 +2,44 @@
 
 (in-package #:aromyxo)
 
-(define-global +lisp-obj->metadata+
-    (make-hash-table :test #'equal)) ;; I'm not sure EQ has what it takes wrt. "range" here.
+(declaim (optimize speed (safety 0)))
+
+
+(define-global +lisp-obj->metadata+ 
+    (make-hash-table :test #'eq :weakness :key))
 
 
 (defun lisp-obj-set-metadata (lisp-obj key value)
-  (let ((hash +lisp-obj->metadata+)
-        (signature (sb-kernel:get-lisp-obj-address lisp-obj)))
+  (let ((hash +lisp-obj->metadata+))
     (sb-ext:with-locked-hash-table (hash)
       (multiple-value-bind (key_value-hash found-p)
-          (gethash signature hash)
+          (gethash lisp-obj hash)
         (if found-p
             (sb-ext:with-locked-hash-table (key_value-hash)
               (setf (gethash key key_value-hash) value))
-            (progn
-              (setf (gethash key (setf (gethash signature hash) (make-hash-table :test #'eq)))
-                    value)
-              (sb-ext:finalize lisp-obj
-                               (lambda ()
-                                 (remhash signature hash)))))))))
+            (setf (gethash key (setf (gethash lisp-obj hash) (make-hash-table :test #'eq)))
+                  value))))))
 (export 'lisp-obj-set-metadata)
 
 
 (defun lisp-obj-get-metadata (lisp-obj key)
-  (let ((hash +lisp-obj->metadata+)
-        (signature (sb-kernel:get-lisp-obj-address lisp-obj)))
+  (let ((hash +lisp-obj->metadata+))
     (sb-ext:with-locked-hash-table (hash)
       (multiple-value-bind (key_value-hash found-p)
-          (gethash signature hash)
+          (gethash lisp-obj hash)
         (if found-p
             (sb-ext:with-locked-hash-table (key_value-hash)
               (gethash key key_value-hash))
             (values nil nil))))))
 (export 'lisp-obj-get-metadata)
-                
+
+
+(defun (setf metadata-of) (value lisp-obj key)
+  (lisp-obj-set-metadata lisp-obj key value))
+(export 'metadata-of)
+
+
+(defun metadata-of (lisp-obj key)
+  (lisp-obj-get-metadata lisp-obj key))
+(export 'metadata-of)
+  
