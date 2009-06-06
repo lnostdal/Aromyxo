14f6a901a1c6acb49edab1bf41d727858eba78c4
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-06-06 17:51:04 +0200

    § is now a dispatch character. I'm not yet sure whether this is a good candidate for this so I might switch to another character later.
    §c is curry.
    §f is funcall.
    §a is apply.
    
    
    ~ also has additional behavior:
    
    AMX> (defun mk-addn (n)
          (lambda (x) (+ x n)))
    MK-ADDN
    AMX> ~((mk-addn 2) 3)
    5
    AMX>
    
    
    Another example:
    
    
    AMX> (let ((times-2-plus-1 (compose §c(+ 1) §c(* 2))))
              (values §f(times-2-plus-1 3)
                      §f(times-2-plus-1 4)
                      §f((compose §c(+ 1) §c(* 2)) 3 4)
                      §a((compose §c(+ 1) §c(* 2)) (list 3 4))
                      ~((compose §c(+ 1) §c(* 2)) 3 4 5)))
    7
    9
    25
    25
    121
    AMX>

--- util/read-macros.lisp
+++ util/read-macros.lisp
@@ -3,6 +3,9 @@
 (in-package #:aromyxo)
 
 
+(make-dispatch-macro-character #\§)
+
+
 (set-dispatch-macro-character #\# #\l
                               (lambda (stream char arg)
                                 (declare (ignore char arg))
@@ -12,7 +15,10 @@
 (set-macro-character #\~
                      (lambda (stream char)
                        (declare (ignore char))
-                       `(deref ,(read stream)))
+                       (let ((form (read stream)))
+                         (if (and (listp form) (listp (first form)))
+                             `(funcall ,(first form) ,@(rest form))
+                             `(deref ,form))))
                      t)
 
 
@@ -23,34 +29,6 @@
                      t)
 
 
-
-#|
-This adds ¤( and ¤ as meaningful constructs to the language.
-
-
-For instance,
-
-  ¤(html-element :model some-model)
-
-
-..is short for,
-
-  (make-instance 'html-element :model some-model).
-
-
-¤ is used in combination with WITH-OBJECT, like this:
-
-  (let ((view ¤(html-element :model some-model)))
-    (with-object view
-      ¤model))
-
-
-..which would be the same as saying:
-
-  (let ((view ¤(html-element :model some-model)))
-    (with-slots (model) view
-      model))
-|#
 (set-macro-character #\¤
                      (lambda (stream char)
                        (declare (ignore char))
@@ -66,13 +44,10 @@ For instance,
                      t)
 
 
-;; TODO: Add support for the ASSERT here based on compiler-settings.
 (set-macro-character #\↑
                      (lambda (stream char)
                        (declare (ignore char))
-                       `(with-object (self) #|(let ((self (self)))
-                                       (assert self nil "AROMYXO: While expanding the ↑ reader macro, (SELF) did not return anything.")
-                                       self)|#
+                       `(with-object (self)
                           ,(read stream)))
                      t)
 
@@ -84,16 +59,22 @@ For instance,
                      t)
 
 
+(set-dispatch-macro-character #\§ #\c
+                              (lambda (stream char arg)
+                                (declare (ignore char arg))
+                                (let ((form (read stream)))
+                                  `(curry ',(first form) ,@(rest form)))))
 
 
-#|
- (set-macro-character #\§
-                     (lambda (stream char)
-                       (declare (ignore char))
-                       (let* ((form (read stream)))
-                         (if (char= #\( (char (princ-to-string form) 0))
-                             `(make-instance ',(first form) ,@(rest form))
-                             (error "AROMYXO: Error in read macro for §, expected a left paren, but got: ~A"
-                                    form))))
-                     t)
-|#
+(set-dispatch-macro-character #\§ #\f
+                              (lambda (stream char arg)
+                                (declare (ignore char arg))
+                                (let ((form (read stream)))
+                                  `(funcall ,(first form) ,@(rest form)))))
+
+
+(set-dispatch-macro-character #\§ #\a
+                              (lambda (stream char arg)
+                                (declare (ignore char arg))
+                                (let ((form (read stream)))
+                                  `(apply ,(first form) ,@(rest form)))))
