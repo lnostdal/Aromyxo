ba73650dc2aff20555d12285e2ed918bcdf2ca2e
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-06-14 00:39:14 +0200

    *delay-gc* → -delay-gc-

--- util/gc.lisp
+++ util/gc.lisp
@@ -3,13 +3,14 @@
 (in-package #:aromyxo)
 
 
-(defvar *delay-gc*
-  (make-hash-table :test #'eq :weakness :value :synchronized nil))
+(define-global -delay-gc-
+    (make-hash-table :test #'eq :weakness :value))
 
 
 (defun delay-gc (until-gc-of-object &rest objects)
   "Keep OBJECTS around for at least as long as UNTIL-GC-OF-OBJECT is around."
-  (sb-ext:with-locked-hash-table (*delay-gc*)
+  (sb-ext:with-locked-hash-table (-delay-gc-)
     (dolist (object objects)
-      (setf (gethash object *delay-gc*)
+      (setf (gethash object -delay-gc-)
             until-gc-of-object))))
+(export 'delay-gc)
