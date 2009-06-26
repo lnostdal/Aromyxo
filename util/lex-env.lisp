c9d9e1e8baafd43ef1b9e9a0cc15c077a6236fbe
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-06-26 02:20:23 +0200

    ...

--- util/lex-env.lisp
+++ util/lex-env.lisp
@@ -2,21 +2,6 @@
 
 (in-package #:aromyxo)
 
-;; from `vixey' @ #lispcafe on freenode .. cool thing
-#|
-
-(let ((x 1))
-  (if (lexically-bound-p y)
-      "y is here"
-      "y is not here"))
-=> y is not here
-
-(let ((y 1))
-  (if (lexically-bound-p y)
-      "y is here"
-      "y is not here"))
-=> y is here
-|#
 
 (defmacro lexically-bound-p (variable &environment env)
   (eq :lexical (sb-cltl2:variable-information variable env)))
