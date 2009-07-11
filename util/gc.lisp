647ae4f513faebddb43f6cbe01ef95d0bd0a9eb0
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-07-11 06:51:40 +0200

    Make the delay-gc stuff a bit simpler...

--- util/gc.lisp
+++ util/gc.lisp
@@ -9,23 +9,18 @@
     :type hash-table)
 
 
-(defun delay-gc (parent object)
-  "Keep OBJECT around for at least as long as PARENT is around."
-  (sb-ext:with-locked-hash-table (-delay-gc-)
-    (setf (gethash object -delay-gc-) parent)))
-(export 'delay-gc)
+(defun delay-gc (parent objects)
+  "Keep OBJECTS around for at least as long as PARENTS is around.
 
-
-(defun delay-gc* (parent objects)
-  "Keep OBJECTS around for at least as long as PARENT is around."
+OBJECTS can be a single object or a list of objects."
   (sb-ext:with-locked-hash-table (-delay-gc-)
-    (dolist (object objects)
+    (dolist (object (ensure-list objects))
       (setf (gethash object -delay-gc-) parent))))
-(export 'delay-gc*)
+(export 'delay-gc)
 
 
 (defmacro with-lifetime (parent &body body)
   "Top-level objects constructed in the lexical scope of BODY will be kept
 around (not GCed) for at least as long as PARENT is around."
-  `(delay-gc* ,parent (list ,@body)))
+  `(delay-gc ,parent (list ,@body)))
 (export 'with-lifetime)
