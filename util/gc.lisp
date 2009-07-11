ab2d403c83174389fecbcb57cac4a3e27786e265
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-07-11 06:11:23 +0200

    Changes to DELAY-GC, and DELAY-GC* was added.
    WITH-LIFETIME macro added and exported.
    
    
    SW-MVC> (defparameter x #λ2)
    X
    SW-MVC> (with-lifetime x
              #λ(format t "SQUARE: ~A~%" (* ~x ~x)))
    SQUARE: 4
    NIL
    SW-MVC> (incf ~x)
    SQUARE: 9
    3
    T
    SW-MVC> (incf ~x)
    SQUARE: 16
    4
    T
    SW-MVC> (sb-ext:gc :full t) (sb-ext:gc :full t) ;; to make sure
    NIL
    SW-MVC> (incf ~x)
    SQUARE: 25
    5
    T
    SW-MVC>

--- util/gc.lisp
+++ util/gc.lisp
@@ -9,10 +9,23 @@
     :type hash-table)
 
 
-(defun delay-gc (until-gc-of-object &rest objects)
-  "Keep OBJECTS around for at least as long as UNTIL-GC-OF-OBJECT is around."
+(defun delay-gc (parent object)
+  "Keep OBJECT around for at least as long as PARENT is around."
+  (sb-ext:with-locked-hash-table (-delay-gc-)
+    (setf (gethash object -delay-gc-) parent)))
+(export 'gc)
+
+
+(defun delay-gc* (parent objects)
+  "Keep OBJECTS around for at least as long as PARENT is around."
   (sb-ext:with-locked-hash-table (-delay-gc-)
     (dolist (object objects)
-      (setf (gethash object -delay-gc-)
-            until-gc-of-object))))
-(export 'delay-gc)
+      (setf (gethash object -delay-gc-) parent))))
+(export 'delay-gc*)
+
+
+(defmacro with-lifetime (parent &body body)
+  "Top-level objects constructed in the lexical scope of BODY will be kept
+around (not GCed) for at least as long as PARENT is around."
+  `(delay-gc* ,parent (list ,@body)))
+(export 'with-lifetime)
