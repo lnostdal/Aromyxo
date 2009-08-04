59f01e6352ab504281348ccf4ae80e2c709c22cc
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-08-04 11:46:24 +0200

    +CR+ +LF+ and +CRLF+ added and exported.

--- web/constants.lisp
+++ web/constants.lisp
@@ -3,6 +3,26 @@
 (in-package #:aromyxo)
 
 
+(define-variable +cr+
+    :kind :constant
+    :value (format nil "~C" #\Return)
+    :test #'string=)
+(export '+cr+)
+
+
+(define-variable +lf+
+    :kind :constant
+    :value (format nil "~C" #\Newline)
+    :test #'string=)
+(export '+lf+)
+
+(define-variable +crlf+
+    :kind :constant
+    :value (format nil "~C~C" #\Return #\Newline)
+    :test #'string=)
+(export '+crlf+)
+
+
 (define-constant +day-names+
     #("Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun")
   :test #'equalp)
