1d4103696e23d36c1c082cb6286b63f27a6cb01a
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-08-19 03:31:39 +0200

    Mostly Cleanups etc...

--- util/object.lisp
+++ util/object.lisp
@@ -25,26 +25,6 @@
 (export '(mk-meta-slot object slot-data-of))
 
 
-#| TODO: This has been deprecated in place for the stuff in metadata.lisp
-(mk-meta-slot properties-of)
-
-(defun property-of (object property-name &key (test #'eq))
-  (let ((result (assoc property-name (properties-of object) :test test)))
-    (if result
-        (values (cdr result) t)
-        (values nil nil))))
-(export 'property-of)
-
-
-(defun (setf property-of) (property-value object property-name &key (test #'eq))
-  (let ((already-existing (assoc property-name (properties-of object) :test test)))
-    (if already-existing
-        (setf (cdr already-existing) property-value)
-        (push (cons property-name property-value) (properties-of object)))))
-(export 'property-of)
-|#
-
-
 (declaim (inline mk-id-generator)
          (ftype (function () (values (cons integer mutex) &optional))
                 mk-id-generator))
@@ -77,55 +57,6 @@
 (export 'id-generator-next-str)
 
 
-#|
-(defvar *object->id*
-  (make-hash-table :test #'eq :weakness :key))
-(export '*object->id*)
-
-;;(mk-meta-slot id-generator-fn-of)
-
-(let ((lock (bt:make-recursive-lock))) ;; Got to be a bit paranoid about this stuff; it cannot be "locked from the outside".
-  (defmethod generate-id-for (object)
-    (declare (optimize speed))
-    (bt:with-recursive-lock-held (lock)
-      (let ((class (class-of object)))
-        (multiple-value-bind (id-generator found-p)
-            (id-generator-fn-of class)
-          (if found-p
-              (funcall (the function id-generator))
-              (prog1 0
-                (setf (id-generator-fn-of class)
-                      (let ((id 0))
-                        (declare (integer id))
-                        (lambda ()
-                          (locally (declare (sb-ext:muffle-conditions sb-ext:compiler-note))
-                            (incf id)))))))))))
-  (export 'generate-id-for)
-
-
-  (defmethod id-of (object)
-    (declare (optimize speed))
-    (bt:with-recursive-lock-held (lock)
-      (multiple-value-bind (id found-p) (gethash object *object->id*)
-        (if found-p
-            id
-            (let ((id (generate-id-for object)))
-              (setf (gethash object *object->id*) id)
-              id)))))
-  (export 'id-of))
-
-
-(defun id-str-of (object)
-  "Convert the ID of OBJECT to a string and try to make it as short (number of characters) as possible."
-  (declare (optimize speed))
-  (write-to-string (id-of object)
-                   :pretty nil
-                   :base 36))
-(export 'id-str-of)
-|#
-
-
-
 
 (define-variable -id-generator-
     :kind :global
@@ -144,13 +75,13 @@
   (setf (slot-value obj 'id)
         (if id-supplied-p
             id
-            (catstr (string (type-of obj)) "-" (id-generator-next-str -id-generator-)))))
+            (id-generator-next-str -id-generator-)
+            ;; TODO: This adds a bit more overhead wrt. bandwidth than I'm comfortable with, but it's still nice
+            ;; wrt. debugging. As always it sucks adding yet another run-time check type of thing...
+            ;; Perhaps debugging should be done by using the TITLE attribute?
+            #|(catstr (string (type-of obj)) "-" (id-generator-next-str -id-generator-))|#)))
 
-(defmethod print-object :around ((id-mixin id-mixin) stream)
-  (print-unreadable-object (id-mixin stream :type t :identity nil)
-    (format stream ":ID ~S" (id-of id-mixin))
-    (call-next-method)))
 
-
-(defmethod print-object ((id-mixin id-mixin) stream)
-  )
+(defmethod print-slots progn ((id-mixin id-mixin) stream)
+  (when (slot-boundp id-mixin 'id)
+    (format stream " :ID ~S" (id-of id-mixin))))
