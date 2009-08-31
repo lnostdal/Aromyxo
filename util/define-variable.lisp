037fe347342ee8a4dcc8e64223eb1263704f2cb1
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-08-31 21:48:07 +0200

    DEFINE-VARIABLE: Ensure that the return values are correct.

--- util/define-variable.lisp
+++ util/define-variable.lisp
@@ -3,6 +3,8 @@
 (in-package #:aromyxo)
 
 
+;; TODO: Do the PROG1 thing here so the return values end up correct.
+
 (defmacro define-variable (name &key
                            (value nil value-supplied-p)
                            (kind (case (char (string name) 0)
@@ -13,36 +15,37 @@
                            (type nil type-supplied-p)
                            (always-boundp value-supplied-p)
                            (test nil test-supplied-p))
-  ;; TODO: Slimes indentation is horrible here.
+  ;; TODO: Slime indentation is horrible here.
   `(progn
-     ,(when type-supplied-p
-            `(eval-now (proclaim '(,type ,name))))
+     ,@(when type-supplied-p
+             `((eval-now (proclaim '(,type ,name)))))
 
-     ,(case kind
-            (:var
-             `(eval-now
-                (defvar ,name
-                  ,@(when (or always-boundp value-supplied-p)
-                          `(,value)))
-                ,@(when doc-supplied-p
-                        `((setf (documentation ',name 'variable) ,doc)))))
+     (prog1
+         ,(case kind
+                (:var
+                 `(eval-now
+                    (prog1 (defvar ,name
+                             ,@(when (or always-boundp value-supplied-p)
+                                     `(,value))))
+                    ,@(when doc-supplied-p
+                            `((setf (documentation ',name 'variable) ,doc)))))
 
-            (:parameter
-             `(eval-now
-                (defparameter ,name ,value
-                  ,@(when doc-supplied-p `(,doc)))))
+                (:parameter
+                 `(eval-now
+                    (defparameter ,name ,value
+                      ,@(when doc-supplied-p `(,doc)))))
 
-            (:constant
-             `(define-constant ,name ,value
-                ,@(when test-supplied-p `(:test ,test))
-                ,@(when doc-supplied-p `(:documentation ,doc))))
+                (:constant
+                 `(define-constant ,name ,value
+                    ,@(when test-supplied-p `(:test ,test))
+                    ,@(when doc-supplied-p `(:documentation ,doc))))
 
-            (:global
-             `(sb-ext:defglobal ,name ,value
-                ,@(when doc-supplied-p `(,doc)))))
+                (:global
+                 `(sb-ext:defglobal ,name ,value
+                    ,@(when doc-supplied-p `(,doc)))))
 
-     ,@(when (and always-boundp (eq kind (or :var :parameter)))
-             `((eval-now (proclaim '(sb-ext:always-bound ,name)))))))
+       ,@(when (and always-boundp (eq kind (or :var :parameter)))
+               `((eval-now (proclaim '(sb-ext:always-bound ,name))))))))
 (export 'define-variable)
 
 
