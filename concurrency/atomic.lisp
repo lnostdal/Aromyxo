e1e6c9389a30b14bf3990de2de7b7b4970a03bd4
Lars Rune Nøstdal <lnostdal@blackbox.(none)>
2009-11-04 12:46:01 +0100

    ATOMIC: Better doc-strings.

--- concurrency/atomic.lisp
+++ concurrency/atomic.lisp
@@ -1,7 +1,7 @@
 ;;;; http://nostdal.org/ ;;;;
 
-(in-package #:aromyxo)
-
+(in-package aromyxo)
+(in-readtable aromyxo)
 (declaim (optimize speed (safety 1)))
 
 
@@ -20,10 +20,8 @@
 (export 'deref)
 
 
-(defmacro swap (place new)
-  "NOTE: This binds %OLD, so this is not a macro intended for direct use by
-outsiders.
-Note that NEW might be evaluated more than once."
+(defmacro %swap (place new)
+  "This binds %OLD. NEW might be evaluated more than once."
   (with-gensyms (mnew)
     `(loop
         (let* ((%old ,place)
@@ -34,19 +32,18 @@ Note that NEW might be evaluated more than once."
 
 (defmacro swap-atom (atom new)
   "NEW must be a value that yields T for (EQ NEW NEW).
-Note that NEW might be evaluated more than once."
+NEW might be evaluated more than once."
   (once-only (atom)
-    `(swap (atomt-value ,atom) ,new)))
+    `(%swap (atomt-value ,atom) ,new)))
 (export 'swap-atom)
 
 
 (declaim (inline swap-atom-fn))
 (defun swap-atom-fn (atom fn)
-  "FN is a function that accepts one argument; the old value of ATOM. It must
-return a value that yields T for (EQ VALUE VALUE). Note that it might be called
-more than once and the old value passed to the function might change for each
-call."
-  (declare (atomt atom)
-           (function fn))
-  (swap-atom atom (funcall fn %old)))
+    "FN is a function that accepts one argument; the old or current value of ATOM.
+It must return a value that yields T for (EQ VALUE VALUE). Note that FN might be
+called more than once and the argument passed to it might change for each call."
+    (declare (atomt atom)
+             (function fn))
+    (swap-atom atom (funcall fn %old)))
 (export 'swap-atom-fn)
