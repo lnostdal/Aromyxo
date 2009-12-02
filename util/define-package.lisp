1d12582f4f113e073c58ee34b4de9b39a644b73d
Lars Rune Nøstdal <lnostdal@blackbox.(none)>
2009-12-02 10:30:30 +0100

    EXPORT-ALL added.

--- util/define-package.lisp
+++ util/define-package.lisp
@@ -4,7 +4,15 @@
 =common-headers=
 
 
-(defun shadowing-use-package (name)
+(defun shadowing-use-package (name &optional (package (sb-int:sane-package)))
   (let ((pkg (find-package name)))
     (do-external-symbols (sym pkg)
-      (shadowing-import sym))))
+      (shadowing-import sym package))))
+
+
+;; TODO: Describe why I do this in some blog-post or something. Short; the point -- besides me being lazy, is that conflicts can be dealt with _once_ and the result/solution trickles down ("inheritance") to sub-sub-sub..-packages.
+(defun export-all (&optional (package (sb-int:sane-package)))
+  (do-symbols (sym package)
+    (export sym package))
+  ;; Dodge CL pitfall.
+  (export '(nil) package))
